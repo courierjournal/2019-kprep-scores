8daead253ba9667cfdb0f0b1adf38caca8545018
Jesse Hazel <jwhazel@jesses-imac.lan>
2019-09-27 00:23:21 -0400

    Create DataTable component

---
+++ src/components/DataTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="table-container">
+    <table>
+      <thead>
+        <tr>
+          <th>School</th>
+          <th>Designation</th>
+          <th>Stars</th>
+          <th>More</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataTable"
+};
+</script>
+
+<style scoped>
+.table-container {
+  padding: 1em;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+thead > tr {
+  border-bottom: 1px solid #ccc;
+}
+
+thead > tr > th {
+  padding: 1em;
+}
+</style>
