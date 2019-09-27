b3e7a9b632304f1d801375c6644ece0eb72f981b
Jesse Hazel <jwhazel@me.com>
2019-09-26 20:40:23 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="app-header">
+      <h1>2019 KPREP test scores</h1>
+    </section>
+    <section class="app-explainer-new"></section>
+    <section class="app-database"></section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
